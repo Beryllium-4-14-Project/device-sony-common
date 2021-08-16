roberto-sartori-gl/4.14-device-oneplus-msm8998-common

rootdir: fix init file names
Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

rootdir/Android.mk
@@ -21,10 +21,10 @@ LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.$(TARGET_DEVICE)
+LOCAL_MODULE := init.cheeseburger
 LOCAL_SRC_FILES := vendor/etc/init/hw/init.common.rc
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := init.$(TARGET_DEVICE)
+LOCAL_MODULE_STEM := init.cheeseburger
 LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init/hw

Beryllium-4-14-Project/device-sony-common

rootdir/Android.mk
@@ -21,10 +21,10 @@ LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.$(TARGET_DEVICE)
+LOCAL_MODULE := init.sdm845
 LOCAL_SRC_FILES := vendor/etc/init/hw/init.common.rc
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_STEM := init.$(TARGET_DEVICE)
+LOCAL_MODULE_STEM := init.sdm845
 LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init/hw